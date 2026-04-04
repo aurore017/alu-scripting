70e8e8c87652ae323cfdb88dfa671ef446d98b8f
aurore017 <a.irakarama1@alustudent.com>
2026-04-04 21:42:31 +0200

    3rd commit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""
-Fetch and print the top 10 hot post titles from a subreddit
-"""
+"""Prints the titles of the first 10 hot posts listed for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "python:top_ten:v1.0 (by /u/anonymous)"}
+    """Queries Reddit API and prints titles of first 10 hot posts."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "mozilla/5.0"}
     params = {"limit": 10}
 
     try:
-        response = requests.get(url, headers=headers,
-                                params=params, allow_redirects=False)
-
-        if response.status_code != 200:
-            print("None")
-            return
-
-        data = response.json().get("data", {}).get("children", [])
-
-        for post in data:
-            print(post.get("data", {}).get("title"))
-
+        response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+        )
     except Exception:
-        print("None")
+        return
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+
+    if not posts:
+        return
+
+    for post in posts:
+        title = post.get("data", {}).get("title")
+        if title:
+            print(title)
